b2c262b691687fd3191080585572cbd549ee77de
zpqqq <2601087520@qq.com>
2021-12-07 20:45:46 +0800

    notdone

---
+++ lab4-notdone/arch/riscv/kernel/clock.c
@@ -0,0 +1,40 @@
+#include"printk.h"
+// QEMU中时钟的频率是10MHz, 也就是1秒钟相当于10000000个时钟周期。
+unsigned long TIMECLOCK = 5000000;
+// unsigned long TIMECLOCK = 10000000;
+
+unsigned long get_cycles()
+{
+    // 使⽤ rdtime 编写内联汇编，获取 time 寄存器中 (也就是mtime 寄存器 )的值并返回
+    unsigned long res; 
+    __asm__ volatile(
+		"rdtime %[res]\n"
+		: [res] "=r"(res)				// output
+		:                               // input 
+        :                               // modification
+	);
+	return res; 
+}
+
+void clock_set_next_event()
+{
+    // 下⼀次 时钟中断 的时间点
+    unsigned long next = get_cycles() + TIMECLOCK;
+    // 使⽤ sbi_ecall 来完成对下⼀次时钟中断的设置
+	printk("%x\n", next); 
+
+    __asm__ volatile(
+		"addi a0, %[next], 0\n"
+		"addi a1, zero, 0\n"
+		"addi a2, zero, 0\n"
+		"addi a3, zero, 0\n"
+		"addi a4, zero, 0\n"
+		"addi a5, zero, 0\n"
+		"addi a6, zero, 0\n"
+		"addi a7, zero, 0\n"
+		"ecall\n"
+		: 			// output
+		: [next] "r" (next)	// input
+        : "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7"
+	);
+}
---
+++ lab4-notdone/arch/riscv/kernel/vm.c
@@ -0,0 +1,109 @@
+#include "defs.h"
+#include "string.h"
+#include "mm.h"
+#include "printk.h"
+unsigned long  early_pgtbl[512] __attribute__((__aligned__(0x1000)));
+extern char _stext[];
+extern char _etext[];
+extern char _srodata[];
+extern char _erodata[];
+extern char _sdata[];
+extern char _edata[];
+extern char _sbss[];
+extern char _ebss[];
+extern char _ekernel[];
+void setup_vm(void)
+{
+    early_pgtbl[VM_START >> 30 & 0x1ff] = ((PHY_START >> 30 & 0x3ffffff) << 28) + (V_MASK | R_MASK | W_MASK | X_MASK);
+    early_pgtbl[PHY_START >> 30 & 0x1ff] = ((PHY_START >> 30 & 0x3ffffff) << 28) + (V_MASK | R_MASK | W_MASK | X_MASK);
+    printk("before: 0x%lx\n", early_pgtbl); 
+    // TODO: find a solution that don't need Equivalent mapping
+    // ! no hurry
+    // *: The purpose for Equivalent mapping is that, the first call to `csrw satp` the context is still under the physical address space(0x8000000)
+    // *: After the set, the context is switch to the virtual address space, but rip is still 0x800000
+    // *: thus,If we don't set the equivalent mapping, the rip will lost itself.
+    return;
+}
+unsigned long swapper_pg_dir[512] __attribute__((__aligned__(0x1000)));
+void create_mapping(unsigned long* pgtbl, unsigned long va, unsigned long pa, unsigned long sz, int perm)
+{
+    // TODO: setup the three level page table map
+    int occu = 0; 
+    // !: sz may be over a pagesize
+    for(int i=0; sz>0; i++){
+        int VPN[3]; 
+        unsigned long *addr2, *addr3; 
+        unsigned long cur_va = va + i*PGSIZE; 
+        // !: one va however multiple pa?
+        VPN[0] = (cur_va) >> 12 & 0x1ff; 
+        VPN[1] = (cur_va) >> 21 & 0x1ff; 
+        VPN[2] = (cur_va) >> 30 & 0x1ff; 
+
+        // first level
+        if(pgtbl[VPN[2]] & V_MASK)
+        {
+            //!: physical?
+            addr2 = (unsigned long)((pgtbl[VPN[2]] >> 10) << 12) + PA2VA_OFFSET; 
+        }
+        else
+        {
+            addr2 = kalloc(); // !: ? 
+            pgtbl[VPN[2]] = ((((unsigned long)addr2 - PA2VA_OFFSET) >> 12) << 10) | V_MASK; 
+        }
+        // second level
+        if(addr2[VPN[1]] & V_MASK)
+        {
+            //!: physical?
+            addr3 = (unsigned long)((addr2[VPN[1]] >> 10) << 12) + PA2VA_OFFSET; 
+        }
+        else
+        {
+            addr3 = kalloc(); // !: ?
+            addr2[VPN[1]] = ((((unsigned long)addr3 - PA2VA_OFFSET) >> 12) << 10) | V_MASK; 
+        }
+        // third level
+        // printk("{%d} VPN[0]: %x; value: %lx ### ", i, VPN[0], (pa >> 12 + i) << 10 + perm); 
+        // printk("%d; %lx; rl: %lx\n", perm, pa, pa >> 12 + i); 
+        addr3[VPN[0]] = (((pa >> 12) + i) << 10) | perm; 
+        occu++; 
+        // printk("%lx ## %d\n", cur_va, occu); 
+        // printk("%x $$ %x $$ %x\n", VPN[2], VPN[1], VPN[0]); 
+        // printk("%lx: %lx\n", pgtbl, pgtbl[VPN[2]]); 
+        // printk("%lx: %lx\n", addr2, addr2[VPN[1]]); 
+        // printk("%lx: %lx\n", addr3, addr3[VPN[0]]); 
+        // printk("------------------\n"); 
+        // map next page
+        sz -= PGSIZE; 
+    }
+    printk("%lx ^^ %x ^^ done\n", pgtbl, occu); 
+}
+void setup_vm_final(void){
+    printk("after: 0x%lx\n", early_pgtbl); 
+    memset(swapper_pg_dir,0x0,PGSIZE);
+    create_mapping(swapper_pg_dir,(unsigned long)_stext,(unsigned long)(PHY_START + OPENSBI_SIZE),(unsigned long)PGSIZE,X_MASK | R_MASK | V_MASK);
+    create_mapping(swapper_pg_dir,(unsigned long)_srodata,(unsigned long)(PHY_START + OPENSBI_SIZE + PGSIZE), (unsigned long)PGSIZE, R_MASK | V_MASK);
+    create_mapping(swapper_pg_dir,(unsigned long)_sdata,(unsigned long)(PHY_START + OPENSBI_SIZE + PGSIZE * 2), (unsigned long)PGSIZE, W_MASK | R_MASK | V_MASK);
+    create_mapping(swapper_pg_dir,(unsigned long)_sbss,(unsigned long)(PHY_START + OPENSBI_SIZE + PGSIZE * 3), (unsigned long)PGSIZE, W_MASK | R_MASK | V_MASK);
+    create_mapping(swapper_pg_dir,(unsigned long)_ekernel, (unsigned long)(PHY_START + OPENSBI_SIZE + PGSIZE * 4),(unsigned long)(PHY_SIZE - 4*PGSIZE),W_MASK | R_MASK | V_MASK);
+
+    printk("tete: %lx\n", swapper_pg_dir);
+    // set the stap register and clear tlb
+    // !: swapper_pgtbl is virtual?
+    __asm__ volatile(
+        // mode
+        "li a0, 0x8\n"
+        "slli a0, a0, 60\n"
+        // PPN
+        "addi a1, %[PPN], 0\n"
+        "srli a1, a1, 12\n"
+        "add a0, a0, a1\n"
+        "csrw satp, a0\n"
+        "sfence.vma zero, zero\n"
+        :   // output
+        : [PPN] "r" ((unsigned long)swapper_pg_dir-PA2VA_OFFSET)   // input
+        : "a0", "a1"   // modification
+        );
+    printk("*********************\n"); 
+    
+    return;
+}
